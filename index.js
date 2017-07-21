abc14a2c32d90feded01ee664504816b7131341e
Brian Gonzalez <me@briangonzalez.org>
2017-07-21 13:48:39 -0700

    move from bower to npm

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 /* jshint node: true */
-const path = require('path');
 'use strict';
+const fastbootTransform = require('fastboot-transform');
 
 module.exports = {
   name: 'ember-shave',
 
-  afterInstall() {
-    return this.addBowerPackageToProject("shave");
+  options: {
+    nodeAssets: {
+      shave: {
+        vendor: {
+          include: ['dist/shave.js'],
+          processTree(input) {
+            return fastbootTransform(input);
+          },
+        },
+      },
+    },
   },
-
-  included(app) {
-    this._super(app);
-
-    if (process.env.EMBER_CLI_FASTBOOT) { return; }
-
-    const shavePath = path.join(app.bowerDirectory, "shave/dist/shave.min.js");
-    app.import(shavePath);
-  }
 };
